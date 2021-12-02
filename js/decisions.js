111fbf1d9f857a290b13dd12f127b3a613536b36
Mark Carlton <mcarlton33@gmail.com>
2021-12-02 09:55:29 +0300

    dom manipulation

--- js/decisions.js
+++ js/decisions.js
@@ -1,103 +1,49 @@
-// // FOR
-// const awesomeArray = ["oranges", "mangoes", "apples"]
-// for (let index = 0; index < awesomeArray.length; index++) {
-//     const element = awesomeArray[index];
-//     // console.log(element.toUpperCase())
+const header = document.children[0].children[1].children[10].textContent;
+console.log(header)
+    // const tillNumberAnalyzer = (message) => {
+    //     // Placeholder variables
+    //     let code, amount, day, till;
+
+//     const initialSplitter = message.split(" Confirmed. ");
+//     code = initialSplitter[0];
+
+//     const rightHandside = initialSplitter[1];
+//     const secondarySplitter = rightHandside.split(" paid to ");
+//     amount = secondarySplitter[0].replace("Ksh", '')
+
+//     till = secondarySplitter[1].split(". on ")[0]
+//     day = secondarySplitter[1].split(". on ")[1].split(".New ")[0];
+//     return { till, day, code, amount };
 // }
 
-// /* this is an example for new snippet extension make by me xD */
-// for (const element of awesomeArray) {
-//     // console.log(element.toUpperCase())
-// }
+// function analyzeUserInput() {
+//     const mpesaMessage = prompt("Enter mpesa message please");
 
-// awesomeArray.forEach(element => {
-//     // console.log(element.toUpperCase())
-// })
-
-// // WHILE
-// // let values = 10;
-// // while (values > 0) {
-// //     console.log("Hello")
-// //     values = values - 1
-// // }
-
-const tillNumberAnalyzer = (message) => {
-    // Placeholder variables
-    let code, amount, day, till
-
-    const initialSplitter = message.split(" Confirmed. ");
-    code = initialSplitter[0];
-
-    const rightHandside = initialSplitter[1];
-    const secondarySplitter = rightHandside.split(" paid to ");
-    amount = secondarySplitter[0].replace("Ksh", '')
-
-    till = secondarySplitter[1].split(". on ")[0]
-    day = secondarySplitter[1].split(". on ")[1].split(".New ")[0];
-    return till;
-}
-
-function analyzeUserInput() {
-    const mpesaMessage = prompt("Enter mpesa message please");
-
-    if (mpesaMessage.includes("paid to")) {
-        // Post feedback using alert
-        alert(tillNumberAnalyzer(mpesaMessage));
-    } else if (mpesaMessage.includes("sent to")) {
-        alert('This is a paybill')
-    } else {
-        alert('This is another transaction type')
-    }
-}
-
-// Get input using prompt
-
-// // FUNCTIONS
-// // Name - tillNumberAnalyzer
-// // Arguments
-// // Callback 
-
-// // Method 1
-
-
-// // Method 2
-// function payBillAnalyzer(message) {
-
-// }
-
-
-
-// class Player {
-//     team = "Derby County";
-//     jerseyNumber = 10;
-//     bootSize = 10;
-
-//     celebration = () => {
-//         return "Hi 5 players";
+//     if (mpesaMessage.includes("paid to")) {
+//         // Post feedback using alert
+//         alert(tillNumberAnalyzer(mpesaMessage));
+//     } else if (mpesaMessage.includes("sent to")) {
+//         alert('This is a paybill')
+//     } else {
+//         alert('This is another transaction type')
 //     }
 // }
 
-// const rooney = new Player();
-// console.log(rooney.celebration())
-
-// class Circle {
-//     pi = 3.1427
-//     radius;
-
-//     constructor(radius) {
-//         this.radius = radius
-//     }
-
-//     getDiameter() {
-//         return this.radius * 2
-//     }
-
-//     getCircumference() {
-//         return 2 * this.pi * this.getDiameter();
-//     }
+// // Function as a statement
+// function Add(num1, num2) {
+//     let sum = num1 + num2;
+//     return sum;
 // }
 
-// const smallCircle = new Circle(5);
-// const bigCircle = new Circle(50);
+// // Function as an expression
+// let add = function a(num1, num2) {
+//     let sum = num1 + num2;
+//     return sum;
+// }
 
-// console.log(bigCircle.getCircumference())
+// // Arrow function
+// var add = (num1, num2) => num1 + num2;
+
+
+// tillNumberAnalyzer()
+// analyzeUserInput()
---
+++ js/dom-demo.js
@@ -0,0 +1,47 @@
+// // const add = (num1, num2) => num1 + num2;
+// // const result = add(5, 8);
+
+// // Method 1
+// let headerTitle = document.children[0].children[1].children[0].children[0].children[0];
+
+// // document.children[0].children[1].children[0].children[0].children[0].textContent = result;
+
+
+// // Method 2
+// var headerTag = document.getElementById("headingTitle")
+// console.log(headerTag);
+
+// // Method3
+// var cardTag = document.getElementsByClassName("card-group")
+// console.log(cardTag)
+
+// // Function for autoreresh
+
+// // Get tag
+// // Update element
+
+// const myinputBox = document.children[0].children[1].children[0].children[0];
+// myinputBox.onInput() = () => {
+//     // Fetch data from DB
+//     myinputBox.textContent = "result from DB"
+// }
+
+const weatherDeterminer = (weather) => {
+    if (typeof weather == String) {
+        if (weather == "Sunny") {
+            return 'Just grab a vest'
+        } else {
+            return 'You might want to bring a jacket'
+        }
+    } else {
+        return 'Please enter a valid value';
+    }
+}
+
+const handleClick = () => {
+    const inputBox = document.getElementById("weatherToday")
+    const inputValue = inputBox.value;
+    const result = weatherDeterminer(inputValue)
+
+    document.getElementById("resultHere").textContent = result;
+}
